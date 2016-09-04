d7861c3c3b36bfa7742a8b7df7a4238257b9c61a
root <root@vmd13810.contabo.host>
2016-09-04 17:33:42 +0200

    new

--- start.sh
+++ start.sh
@@ -1,37 +1,148 @@
 #!/bin/bash
-#if [ ! -f /wordpress-db-pw.txt ]; then
-#    #mysql has to be started this way as it doesn't work to call from /etc/init.d
-#    /usr/bin/mysqld_safe &
-#    sleep 10s
-#    # Here we generate random passwords (thank you pwgen!). The first two are for mysql users, the last batch for random keys in wp-config.php
-#    MYSQL_PASSWORD=`pwgen -c -n -1 12`
-#    WORDPRESS_PASSWORD=`pwgen -c -n -1 12`
-#    #This is so the passwords show up in logs.
-#    echo mysql root password: $MYSQL_PASSWORD
-#    echo wordpress password: $WORDPRESS_PASSWORD
-#    echo $MYSQL_PASSWORD > /mysql-root-pw.txt
-#    echo $WORDPRESS_PASSWORD > /wordpress-db-pw.txt
-#
-#    mysqladmin -u root password $MYSQL_PASSWORD
-#    mysql -uroot -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-#    mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY '$WORDPRESS_PASSWORD'; FLUSH PRIVILEGES;"
-#    killall mysqld
-#fi
+set -e
 
-if [ ! -f /usr/share/nginx/www/wp-config.php ]; then
-#    WORDPRESS_DB="wordpress"
-#    WORDPRESS_PASSWORD=`cat /wordpress-db-pw.txt`
-#    sed -e "s/database_name_here/$WORDPRESS_DB/
-#    s/username_here/$WORDPRESS_DB/
-#    s/password_here/$WORDPRESS_PASSWORD/
-#    /'AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-#    /'SECURE_AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-#    /'LOGGED_IN_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-#    /'NONCE_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-#    /'AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-#    /'SECURE_AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-#    /'LOGGED_IN_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-#    /'NONCE_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/" /usr/share/nginx/www/wp-config-sample.php > /usr/share/nginx/www/wp-config.php
+if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
+	: "${WORDPRESS_DB_HOST:=mysql}"
+	# if we're linked to MySQL and thus have credentials already, let's use them
+	: ${WORDPRESS_DB_USER:=${MYSQL_ENV_MYSQL_USER:-root}}
+	if [ "$WORDPRESS_DB_USER" = 'root' ]; then
+		: ${WORDPRESS_DB_PASSWORD:=$MYSQL_ENV_MYSQL_ROOT_PASSWORD}
+	fi
+	: ${WORDPRESS_DB_PASSWORD:=$MYSQL_ENV_MYSQL_PASSWORD}
+	: ${WORDPRESS_DB_NAME:=${MYSQL_ENV_MYSQL_DATABASE:-wordpress}}
+
+	if [ -z "$WORDPRESS_DB_PASSWORD" ]; then
+		echo >&2 'error: missing required WORDPRESS_DB_PASSWORD environment variable'
+		echo >&2 '  Did you forget to -e WORDPRESS_DB_PASSWORD=... ?'
+		echo >&2
+		echo >&2 '  (Also of interest might be WORDPRESS_DB_USER and WORDPRESS_DB_NAME.)'
+		exit 1
+	fi
+
+	if ! [ -e index.php -a -e wp-includes/version.php ]; then
+		echo >&2 "WordPress not found in $(pwd) - copying now..."
+		if [ "$(ls -A)" ]; then
+			echo >&2 "WARNING: $(pwd) is not empty - press Ctrl+C now if this is an error!"
+			( set -x; ls -A; sleep 10 )
+		fi
+		tar cf - --one-file-system -C /usr/src/wordpress . | tar xf -
+		echo >&2 "Complete! WordPress has been successfully copied to $(pwd)"
+	fi
+
+	# TODO handle WordPress upgrades magically in the same way, but only if wp-includes/version.php's $wp_version is less than /usr/src/wordpress/wp-includes/version.php's $wp_version
+
+	# version 4.4.1 decided to switch to windows line endings, that breaks our seds and awks
+	# https://github.com/docker-library/wordpress/issues/116
+	# https://github.com/WordPress/WordPress/commit/1acedc542fba2482bab88ec70d4bea4b997a92e4
+	sed -ri 's/\r\n|\r/\n/g' wp-config*
+
+	if [ ! -e wp-config.php ]; then
+		awk '/^\/\*.*stop editing.*\*\/$/ && c == 0 { c = 1; system("cat") } { print }' wp-config-sample.php > wp-config.php <<'EOPHP'
+// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+EOPHP
+		chown wordpress:www-data wp-config.php
+	fi
+
+	# see http://stackoverflow.com/a/2705678/433558
+	sed_escape_lhs() {
+		echo "$@" | sed 's/[]\/$*.^|[]/\\&/g'
+	}
+	sed_escape_rhs() {
+		echo "$@" | sed 's/[\/&]/\\&/g'
+	}
+	php_escape() {
+		php -r 'var_export(('$2') $argv[1]);' "$1"
+	}
+	set_config() {
+		key="$1"
+		value="$2"
+		var_type="${3:-string}"
+		start="(['\"])$(sed_escape_lhs "$key")\2\s*,"
+		end="\);"
+		if [ "${key:0:1}" = '$' ]; then
+			start="^(\s*)$(sed_escape_lhs "$key")\s*="
+			end=";"
+		fi
+		sed -ri "s/($start\s*).*($end)$/\1$(sed_escape_rhs "$(php_escape "$value" "$var_type")")\3/" wp-config.php
+	}
+
+	set_config 'DB_HOST' "$WORDPRESS_DB_HOST"
+	set_config 'DB_USER' "$WORDPRESS_DB_USER"
+	set_config 'DB_PASSWORD' "$WORDPRESS_DB_PASSWORD"
+	set_config 'DB_NAME' "$WORDPRESS_DB_NAME"
+
+	# allow any of these "Authentication Unique Keys and Salts." to be specified via
+	# environment variables with a "WORDPRESS_" prefix (ie, "WORDPRESS_AUTH_KEY")
+	UNIQUES=(
+		AUTH_KEY
+		SECURE_AUTH_KEY
+		LOGGED_IN_KEY
+		NONCE_KEY
+		AUTH_SALT
+		SECURE_AUTH_SALT
+		LOGGED_IN_SALT
+		NONCE_SALT
+	)
+	for unique in "${UNIQUES[@]}"; do
+		eval unique_value=\$WORDPRESS_$unique
+		if [ "$unique_value" ]; then
+			set_config "$unique" "$unique_value"
+		else
+			# if not specified, let's generate a random value
+			current_set="$(sed -rn "s/define\((([\'\"])$unique\2\s*,\s*)(['\"])(.*)\3\);/\4/p" wp-config.php)"
+			if [ "$current_set" = 'put your unique phrase here' ]; then
+				set_config "$unique" "$(head -c1M /dev/urandom | sha1sum | cut -d' ' -f1)"
+			fi
+		fi
+	done
+
+	if [ "$WORDPRESS_TABLE_PREFIX" ]; then
+		set_config '$table_prefix' "$WORDPRESS_TABLE_PREFIX"
+	fi
+
+	if [ "$WORDPRESS_DEBUG" ]; then
+		set_config 'WP_DEBUG' 1 boolean
+	fi
+
+	TERM=dumb php -- "$WORDPRESS_DB_HOST" "$WORDPRESS_DB_USER" "$WORDPRESS_DB_PASSWORD" "$WORDPRESS_DB_NAME" <<'EOPHP'
+<?php
+// database might not exist, so let's try creating it (just to be safe)
+$stderr = fopen('php://stderr', 'w');
+// https://codex.wordpress.org/Editing_wp-config.php#MySQL_Alternate_Port
+//   "hostname:port"
+// https://codex.wordpress.org/Editing_wp-config.php#MySQL_Sockets_or_Pipes
+//   "hostname:unix-socket-path"
+list($host, $socket) = explode(':', $argv[1], 2);
+$port = 0;
+if (is_numeric($socket)) {
+	$port = (int) $socket;
+	$socket = null;
+}
+$maxTries = 10;
+do {
+	$mysql = new mysqli($host, $argv[2], $argv[3], '', $port, $socket);
+	if ($mysql->connect_error) {
+		fwrite($stderr, "\n" . 'MySQL Connection Error: (' . $mysql->connect_errno . ') ' . $mysql->connect_error . "\n");
+		--$maxTries;
+		if ($maxTries <= 0) {
+			exit(1);
+		}
+		sleep(3);
+	}
+} while ($mysql->connect_error);
+if (!$mysql->query('CREATE DATABASE IF NOT EXISTS `' . $mysql->real_escape_string($argv[4]) . '`')) {
+	fwrite($stderr, "\n" . 'MySQL "CREATE DATABASE" Error: ' . $mysql->error . "\n");
+	$mysql->close();
+	exit(1);
+}
+$mysql->close();
+EOPHP
+fi
+
 
 
   # Add https proxy support for wordpress
@@ -47,7 +158,7 @@ if [ ! -f /usr/share/nginx/www/wp-config.php ]; then
   # Download wp-super-cache plugin
   curl -O `curl -i -s https://wordpress.org/plugins/wp-super-cache/ | egrep -o "https://downloads.wordpress.org/plugin/[^']+"`
   unzip -o wp-super-cache.*.zip -d /usr/share/nginx/www/wp-content/plugins
-  chown -R www-data:www-data /usr/share/nginx/www/wp-content/plugins
+  chown -R wordpress:www-data /usr/share/nginx/www/wp-content/plugins
 
   # Delete the .zip files
   rm autoptimize.*.zip nginx-helper.*.zip wp-super-cache.*.zip
@@ -74,9 +185,7 @@ add_filter( 'auto_update_plugin', '__return_true' );
 add_filter( 'auto_update_theme', '__return_true' );
 ENDL
 
-    chown -R wordpress: /usr/share/nginx/www/
-
-fi
+  chown -R wordpress:www-data /usr/share/nginx/www/
 
 # start all the services
 /usr/local/bin/supervisord -n -c /etc/supervisord.conf
