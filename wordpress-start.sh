176b3ddd02856571f7d3ff72abf9028084bdeae5
Nurian <gregorius.stahlbart@gmail.com>
2017-06-07 17:10:51 +0200

    bug fix folder name

--- wordpress-start.sh
+++ wordpress-start.sh
@@ -13,7 +13,8 @@ echo "root:$PRIMEHOST_PASSWORD" | chpasswd
 cd /usr/share/nginx/ \
    && curl -o latest.tar.gz -fSL "https://wordpress.org/latest.tar.gz" \
    && tar xvf latest.tar.gz \
-   && rm latest.tar.gz
+   && rm latest.tar.gz \
+   && rm -r /usr/share/nginx/www
 
 mv /usr/share/nginx/wordpress /usr/share/nginx/www \
     && chown -R www-data:www-data /usr/share/nginx/www \
